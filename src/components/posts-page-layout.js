5060bfe9b8a65288a53ecdb815bed08bfede141a
Monilito Castro <mannycastro.epic@gmail.com>
2021-08-11 00:22:12 -0400

    contentful embedded assets are not populating

--- src/components/posts-page-layout.js
+++ src/components/posts-page-layout.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { BLOCKS, MARKS } from '@contentful/rich-text-types';
 import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
-import { Bold, Image, Text } from "./common/bundle";
+import { Bold, Text } from "./common/bundle";
 
 
 
@@ -12,13 +12,10 @@ const renderOption = {
       [MARKS.BOLD]: text => <Bold>{text}</Bold>,
     },
     renderNode: {
-      [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
-        return (<Image
-          src={`https:${node.data.target.fields.file.url}`}
-          height={node.data.target.fields.file.details.image.height}
-          width={node.data.target.fields.file.details.image.width}
-        />)
-      },
+    //   [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+    //     return (<EmbeddedMedia sysId={node.data.target.sys.id}
+    //     />)
+    //   },
       [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
     }
   }
@@ -28,12 +25,12 @@ const BlogPost = ({ data }) => {
         <>
             {
                 data.allContentfulBlogPost.edges.map(({node})=>{
-
+                    console.log("NODE",node)
                     return (
                         <>
                             <Layout pageTitle={node.title}>
                                 <p>{node.createdAt}</p>
-                                {documentToReactComponents(JSON.parse(node.body.raw))}
+                                {documentToReactComponents(JSON.parse(node.body.raw), renderOption)}
                             </Layout>
                         </>
                     )
@@ -56,6 +53,7 @@ export const pageQuery = graphql`
           title
           updatedAt
           createdAt
+          children {id}
           id
         }
       }
