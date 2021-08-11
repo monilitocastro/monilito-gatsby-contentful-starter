2cea91017abf90ac1bdefea3d59d94227e020bd8
Monilito Castro <mannycastro.epic@gmail.com>
2021-08-11 01:48:47 -0400

    fixed major contentful plot hole

--- src/components/posts-page-layout.js
+++ src/components/posts-page-layout.js
@@ -1,26 +1,36 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { BLOCKS, MARKS } from '@contentful/rich-text-types';
 import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
 import { Bold, Text } from "./common/bundle";
+import { StaticImage } from "gatsby-plugin-image";
 
-
+const assets = {}
 
 const renderOption = {
     renderMark: {
       [MARKS.BOLD]: text => <Bold>{text}</Bold>,
     },
     renderNode: {
-    //   [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
-    //     return (<EmbeddedMedia sysId={node.data.target.sys.id}
-    //     />)
-    //   },
+      [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+        const id = node.data.target.sys.id;
+        if(assets[id] ){
+          const {url, height, width} = assets[id];
+          return (<img src={url} height={height} width={width}/>)
+        }
+        return <></>
+        
+      },
       [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
     }
   }
 
 const BlogPost = ({ data }) => {
+    data.allContentfulAsset.edges.map(({node})=>{
+      const {file:{url, contentType, details:{image:{height, width}}}, contentful_id} = node;
+      assets[contentful_id] = {url, contentType, height, width}
+    })
     return (
         <>
             {
@@ -55,6 +65,23 @@ export const pageQuery = graphql`
           createdAt
           children {id}
           id
+        }
+      }
+    }
+    allContentfulAsset {
+      edges {
+        node {
+          file {
+            url
+            contentType
+            details {
+              image {
+                height
+                width
+              }
+            }
+          }
+          contentful_id
         }
       }
     }
